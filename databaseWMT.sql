998275807845cd25b9a9eb38259bad8fcdf6ac7e
andyp888 <andyphee888@gmail.com>
2015-01-15 21:35:57 +0000

    Updated DB
    Made some final changes and added some test data to make sure it works
    properly

--- databaseWMT.sql
+++ databaseWMT.sql
@@ -1,7 +1,14 @@
+DROP TABLE CustomerStock;
+DROP TABLE TransactionHistory;
+DROP TABLE BankAccount;
+DROP TABLE Meetings;
+DROP TABLE Customer;
+DROP TABLE FinancialAdvisor;
+
 CREATE TABLE FinancialAdvisor (
 faID INT PRIMARY KEY AUTO_INCREMENT,
 faFirstName VARCHAR(25) NOT NULL,
-faLast VARCHAR(25) NOT NULL,
+faLastName VARCHAR(25) NOT NULL,
 faPhoneNo VARCHAR(25) NOT NULL,
 faAddress VARCHAR(225) NOT NULL
 )ENGINE=INNODB;
@@ -18,36 +25,57 @@ FOREIGN KEY (faID) REFERENCES FinancialAdvisor(faID)
 
 
 CREATE TABLE BankAccount (
-accountID INT NOT NULL,
+accountNumber INT NOT NULL,
 customerID INT NOT NULL,
 iban BIGINT NOT NULL,
 bankName VARCHAR(25) NOT NULL,
 bankAddress VARCHAR(225) NOT NULL,
-accountNumber BIGINT NOT NULL,
 balance INT NOT NULL,
-PRIMARY KEY (accountID),
+PRIMARY KEY (accountNumber),
 FOREIGN KEY (customerID) REFERENCES Customer(customerID) 
 )ENGINE=INNODB;
 
 
-
 CREATE TABLE TransactionHistory (
-historyRef INT NOT NULL,
+historyRef INT AUTO_INCREMENT,
 customerID INT NOT NULL,
 dateOfTrans DATE NOT NULL,
-accountID INT NOT NULL,
+accountNum INT NOT NULL,
 faID INT NOT NULL,
-PRIMARY KEY (historyRef, customerID),
+PRIMARY KEY (historyRef),
 FOREIGN KEY (customerID) REFERENCES Customer(customerID),
 FOREIGN KEY (faID) REFERENCES FinancialAdvisor(faID),
-FOREIGN KEY (accountID) REFERENCES BankAccount(accountID)  
+FOREIGN KEY (accountNum) REFERENCES BankAccount(accountNumber)  
 )ENGINE=INNODB;
 
 CREATE TABLE CustomerStock (
-customerID INT PRIMARY KEY,
-company VARCHAR(25) NOT NULL,
+customerID INT,
+company VARCHAR(4) NOT NULL,
 sharesNo INT NOT NULL,
 historyRef INT NOT NULL,
+PRIMARY KEY (customerID,historyRef),
 FOREIGN KEY (customerID) REFERENCES Customer(customerID),
 FOREIGN KEY (historyRef) REFERENCES TransactionHistory(historyRef)
 )ENGINE=INNODB;
+
+CREATE TABLE Meetings (
+meetingRef INT PRIMARY KEY AUTO_INCREMENT,
+meetingDate DATE NOT NULL,
+meetingTime TIME NOT NULL,
+meetingDescription VARCHAR(225),
+meetingCustomer INT NOT NULL,
+FOREIGN KEY (meetingCustomer) REFERENCES Customer(customerID)
+)ENGINE=INNODB;
+
+INSERT INTO FinancialAdvisor (faFirstName,faLastName,faPhoneNo,faAddress) VALUES ('Rob','Pooley',07898334332,'11 Grove Street, Compton, USA');
+INSERT INTO FinancialAdvisor (faFirstName,faLastName,faPhoneNo,faAddress) VALUES ('Alan','Smith',07364756335,'2 Mulholland Drive, London, UK');
+
+INSERT INTO Customer (firstName,lastName,phoneNumber,address,faID) VALUES ('Walter','White',07838374733,'2 Mulholland Drive, London, UK',1);
+
+INSERT INTO BankAccount (accountNumber,customerID,iban,bankName,bankAddress,balance) VALUES (1432123,1,12342345,'Large Bank lol','141 Waterloo Street, Glasgow, UK',5000);
+
+INSERT INTO TransactionHistory (customerID,dateOfTrans,accountNum,faID) VALUES (1,'2014-11-24',1432123,1);
+
+INSERT INTO CustomerStock VALUES (1,'GOOG',3000,1);
+
+INSERT INTO Meetings (meetingDate,meetingTime,meetingDescription,meetingCustomer) VALUES ('2015-02-11','13:30:00','Meeting to discuss progress of investments',1);  
