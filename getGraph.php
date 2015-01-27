06b93bda193d70bb344e0b4c4ad14df2b320b678
gm254 <gary.matthews48@googlemail.com>
2015-01-27 15:19:50 +0000

    Changed some stuff

--- getGraph.php
+++ getGraph.php
@@ -1,8 +1,8 @@
 <?php
-function getGraph($symbol)
+public function getGraph($symbol)
 {
-  $BASE_URL = "http://chart.finance.yahoo.com/z?";
-  $chart_url = $BASE_URL."?s=".$symbol."&t=6m&q=l&l=on&z=l";
+  $BASE_URL = "http://chart.finance.yahoo.com/z?&?s=";
+  $chart_url = $BASE_URL.$symbol."&t=6m&q=l&l=on&z=l";
   return $chart_url;
 }
 ?>
