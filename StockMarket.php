c530202cae8be4ee21712afd72f35022b6dab6d4
gm254 <gary.matthews48@googlemail.com>
2015-01-28 13:36:10 +0000

    Added title

--- StockMarket.php
+++ StockMarket.php
@@ -2,6 +2,7 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<title>Stock Market</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta name="keywords" content="" />
 	<meta name="description" content="" />
