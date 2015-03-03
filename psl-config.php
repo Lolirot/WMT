d7a56f5d61aa92af2d6c48bff20d9307f540256d
Shin <shinwwwxg.wxg@gmail.com>
2015-03-03 19:03:42 +0000

    Create psl-config.php
    Something support Log in page

---
+++ psl-config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * These are the database login details
+ */
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "ap307");    // The database username.
+define("PASSWORD", "abcap307354");    // The database password.
+define("DATABASE", "mysql-server-1");    // The database name.
+
+?>
