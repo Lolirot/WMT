736406d5012b01cc3c103ac22292c75b17a85f04
Lolirot <innavsslv@gmail.com>
2015-03-23 13:42:40 +0000

    Added buttons for FA list and trans History

--- index.php
+++ index.php
@@ -1,33 +1,26 @@
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<link href="default.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="calendar.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="bootstrap-3.3.1/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="screen" />
-<link rel="stylesheet" href="bootstrap-3.3.1/dist/css/bootstrap-theme.min.css" />
-<script src="http://code.jquery.com/jquery-latest.js"></script>
-<script src="bootstrap-3.3.1/js/dropdown.js"></script>
-<script src="bootstrap-3.3.1/js/button.js"></script>
-<script src="bootstrap-3.3.1/js/collapse.js"></script>
-
-<script src="bootstrap-3.3.1/calendar.js"></script>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	<link href="default.css" rel="stylesheet" type="text/css" media="screen" />
 
 
 
-
+<link rel="icon" href="images/favicon.ico"/>
 
 </head>
 <body>
 <!-- start header -->
 <div id="header">
-<a href="index.php"> <img src="images/logo.png" alt="Smiley face"></img></a>
-<p>Wealth Management Tool</p>
+<a href="index.php"><img src="images/WMT.png" alt="Smiley face"></img></a>
 <!-- end header -->
 </div>
 
@@ -47,13 +40,15 @@
   </div>
   <div class="navbar-collapse collapse">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="index.php">Home</a></li>
-      <li ><a href="#about">Clients</a></li>
-      <li ><a href="StockMarket.html">Stock Market</a></li>
-      <li ><a href="transHistory.php">Transactions History</a></li>
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="Clients.php">Clients</a></li>
+      <li><a href="StockMarket.html">Stock Market</a></li>
+      i ><a href="transHistory.php">Transactions History</a></li>
       <li><a href = "falist.php">FA List</a></li>
-      
     </ul>
+   <ul class="nav navbar-nav navbar-right">
+        <li><a href="Login.html">Log out</a></li>
+   </ul>
   </div>
 </nav>
 
@@ -61,18 +56,57 @@
 <div class="row">
 
 
-<div class="col-xs-6 col-md-2">
+<div class="col-md-3">
 <div class="panel panel-default">
   <div class="panel-heading">
-    <h3 class="panel-title">Latest Tweets</h3>
+    <h3 class="panel-title">Newsupdate</h3>
   </div>
   <div class="panel-body">
-                           <a class="twitter-timeline"  href="https://twitter.com/Lolirotten_" data-widget-id="556888036668358656">Tweets by @Lolirotten_</a>
-            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-          
-          
+   <!-- start feedwind code --><script type="text/javascript">document.write('\x3Cscript type="text/javascript" src="' + ('https:' == document.location.protocol ? 'https://' : 'http://') + 'feed.mikle.com/js/rssmikle.js">\x3C/script>');</script><script type="text/javascript">(function() {var params = {rssmikle_url: "https://uk.finance.yahoo.com/news/sector-financial-services/?format=rss",rssmikle_frame_width: "430",rssmikle_frame_height: "600",frame_height_by_article: "",rssmikle_target: "_blank",rssmikle_font: "Arial, Helvetica, sans-serif",rssmikle_font_size: "12",rssmikle_border: "off",responsive: "off",rssmikle_css_url: "",text_align: "left",text_align2: "left",corner: "off",scrollbar: "on",autoscroll: "on",scrolldirection: "up",scrollstep: "3",mcspeed: "20",sort: "New",rssmikle_title: "on",rssmikle_title_sentence: "",rssmikle_title_link: "",rssmikle_title_bgcolor: "#0066FF",rssmikle_title_color: "#FFFFFF",rssmikle_title_bgimage: "",rssmikle_item_bgcolor: "#FFFFFF",rssmikle_item_bgimage: "",rssmikle_item_title_length: "55",rssmikle_item_title_color: "#74C4BD",rssmikle_item_border_bottom: "on",rssmikle_item_description: "on",item_link: "off",rssmikle_item_description_length: "150",rssmikle_item_description_color: "#666666",rssmikle_item_date: "gl1",rssmikle_timezone: "Etc/GMT",datetime_format: "%b %e, %Y %l:%M:%S %p",item_description_style: "text+tn",item_thumbnail: "full",article_num: "15",rssmikle_item_podcast: "off",keyword_inc: "",keyword_exc: ""};feedwind_show_widget_iframe(params);})();</script><div style="font-size:10px; text-align:center; width:430px;"><a href="http://feed.mikle.com/" target="_blank" style="color:#CCCCCC;">RSS Feed Widget</a><!--Please display the above link in your web page according to Terms of Service.--></div><!-- end feedwind code -->
   </div>
 </div>
+</div>
+
+
+
+<div class="col-md-6">
+	<div class="jquery-calendar"></div> 
+	<div class="panel panel-default events">
+  <div class="panel-heading">
+    <h3 class="panel-title">Meetings for this day</h3>
+  </div>
+  <div class="panel-body">
+    Time: </br>
+    Day: </br>
+    Note:
+  </div>
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Upcoming Events</h3>
+  </div>
+  <div class="panel-body">
+    Display here events that are picked from the database, but selected for a specific date or range of dates or something.<br/><br/>
+    <a href="calendarPage.html">View Full Calendar</a>
+    </div>
+
+    </div>
+  </div>
+</div>
+</div>
+
+
+
 
 </div>
 
+
+<div id="footer">
+	<p class="links"> <a href="http://validator.w3.org/check/referer" class="xhtml" title="This page validates as XHTML">Valid <abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a> &nbsp;&bull;&nbsp; <a href="http://jigsaw.w3.org/css-validator/check/referer" class="css" title="This page validates as CSS">Valid <abbr title="Cascading Style Sheets">CSS</abbr></a> </p>
+</div>
+<!-- end footer -->
+</body>
+</html>
