73987820aeb3f958d0453e8bb0a7128d811e42eb
andyp888 <andyphee888@gmail.com>
2015-03-23 14:56:36 +0000

    updated connection file

--- conn.php
+++ conn.php
@@ -2,11 +2,11 @@
 /*****************************
 *Database connection
 *****************************/
-$conn = mysql_connect("localhost","user","password");
+$conn = mysql_connect("mysql-server-1","ap307","abcap307354");
 if (!$conn){
     die("Fail to connect to database:" . mysql_error());
 }
-mysql_select_db("fa", $conn);
+mysql_select_db("ap307", $conn);
 
 mysql_query("set character set 'utf-8'");
 
